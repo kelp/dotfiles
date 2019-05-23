7235f647173b82772e2d59256d5aa96e626e25b9
Travis Cole <11240+kelp@users.noreply.github.com>
2019-05-23 03:47:03 +0000

    Add neofetch and motd configs

---
+++ .config/motd/motd.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/fish
+#
+# Generate a nice motd
+#
+
+set TMP (mktemp)
+set MOTD $HOME/.config/motd/motd
+
+neofetch --ascii_distro arch_small >> $TMP
+
+echo "Updates: " >> $TMP
+checkupdates >> $TMP
+echo "Last: " >> $TMP
+last -n 2 | grep -v wtmp | sed '/^$/d'  >> $TMP
+
+mv $TMP $HOME/.config/motd/motd
+
+rm -f $TMP
